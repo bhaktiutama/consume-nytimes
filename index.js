5dbec9c87d9f62373d766d194c751dc779211d3d
bhaktiutama <bhakti.utama@gmail.com>
2021-02-05 01:31:16 +0700

    major update

--- index.js
+++ index.js
@@ -2,19 +2,70 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const axios = require("axios").default
 
-// api access
+
+// template engine
+app.set('view engine', 'ejs');
+
+// simplified api 
 var api = require("./routes/api.router.js")
 app.use("/api", api)
 
-// render
-app.get("/", (req, res) => {
-    res.json({ message: "Hello NYTimes" })
-});
+// render index-article
+app.get('/', function(req, res) {
+    var query = req.query.query
+    var sort = "newest"
+    if (req.query.sort != null) {
+        sort = req.query.sort
+    }
+    var page = 0
+    if (req.query.page != null) {
+        page = req.query.page
+    } 
+
+    // request data to api
+    const host = process.env.HOST+":"+process.env.PORT
+    axios.get(host+"/api/search?query="+query+"&sort="+sort+"&page="+page).then((response) => {
+        var result = response.data.data
+        res.render('pages/index', {result : result})
+    })
+})
+
+
+// render book
+app.get('/book', function(req, res) {
+    var list = "hardcover-fiction" 
+    if (req.query.list != null) {
+        list = req.query.list
+    }
+    var offset = 0
+    if (req.query.offset!=null) {
+        offset = req.query.offset
+    }
+
+    const host = process.env.HOST+":"+process.env.PORT
+    function getListName() {
+        return axios.get(host+"/api/book/listname");
+      }
+      
+    function getBooks() {
+        return axios.get(host+"/api/book?list="+list+"&offset="+offset);
+    }
+      
+    // multiple request
+    Promise.all([getListName(), getBooks()]).then(function (results) {
+          var listname = results[0];
+          var book = results[1];
+          res.render('pages/book', {listname : listname.data.data, book : book.data})
+    });    
+
+
+})
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
-});
+})
 
--- routes/api.router.js
+++ routes/api.router.js
@@ -4,6 +4,7 @@ const axios = require("axios").default
 const key = process.env.KEY
 var url = process.env.APIURL
 
+// api route for search article
 router.get('/search', function (req, res) {
 
     var query = req.query.query
@@ -11,11 +12,13 @@ router.get('/search', function (req, res) {
     var page = req.query.page
 
     // build url based nytimes api
+    var endpoint="/svc/search/v2/articlesearch.json"
     var param = "?api-key="+key+"&q="+query+"&sort="+sort+"&page="+page
-    console.log(url+param)
+    console.log(url+endpoint+param)
 
-    axios.get(url+param).then((response) => {
+    axios.get(url+endpoint+param).then((response) => {
 
+        // transform json
         var article = response.data.response.docs.map(article => {
             return {
                 web_url: article.web_url,
@@ -35,11 +38,53 @@ router.get('/search', function (req, res) {
         
         res.send(result)
     })
-
 })
 
+// api route for list books
 router.get('/book', function (req, res) {
-    res.send('Book')
+
+    var list = req.query.list
+    var offset = req.query.offset
+
+    // build url based nytimes api
+    var endpoint="/svc/books/v3/lists.json"
+    var param = "?api-key="+key+"&list="+list+"&offset="+offset
+    console.log(url+endpoint+param)
+
+    axios.get(url+endpoint+param).then((response) => {
+
+        // transform json
+        var result = response.data.results.map(book => {
+            return book.book_details[0]
+        })
+        res.send(result)
+
+    })    
+
 })
+
+// api route for list books
+router.get('/book/listname', function (req, res) {
+
+    // build url based nytimes api
+    var endpoint="/svc/books/v3/lists/names.json"
+    var param = "?api-key="+key
+    console.log(url+endpoint+param)
+
+    axios.get(url+endpoint+param).then((response) => {
+
+        // transform json
+        var result = response.data.results.map(list => {
+            return {
+                list_name : list.list_name,
+                list_name_encoded : list.list_name_encoded
+            }
+        })
+        res.send({data : result})
+        
+    })    
+
+})
+
   
 module.exports = router
